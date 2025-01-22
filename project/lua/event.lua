b01a05fe95cffa85944dbfb4f3960204b164c580
Ádám Knapecz <knapecz.adam@gmail.com>
2025-01-22 14:02:49 +0100

    feat: add keycode enum

---
+++ project/lua/event.lua
@@ -0,0 +1,36 @@
+-- event.lua
+KeyCode = {}
+KeyCode.__index = KeyCode
+
+KeyCode.A = 0x41
+KeyCode.B = 0x42
+KeyCode.C = 0x43
+KeyCode.D = 0x44
+KeyCode.E = 0x45
+KeyCode.F = 0x46
+KeyCode.G = 0x47
+KeyCode.H = 0x48
+KeyCode.I = 0x49
+KeyCode.J = 0x4A
+KeyCode.K = 0x4B
+KeyCode.L = 0x4C
+KeyCode.M = 0x4D
+KeyCode.N = 0x4E
+KeyCode.O = 0x4F
+KeyCode.P = 0x50
+KeyCode.Q = 0x51
+KeyCode.R = 0x52
+KeyCode.S = 0x53
+KeyCode.T = 0x54
+KeyCode.U = 0x55
+KeyCode.V = 0x56
+KeyCode.W = 0x57
+KeyCode.X = 0x58
+KeyCode.Y = 0x59
+KeyCode.Z = 0x5A
+KeyCode.LeftArrow = 0x25
+KeyCode.UpArrow = 0x26
+KeyCode.RightArrow = 0x27
+KeyCode.DownArrow = 0x28
+
+return KeyCode

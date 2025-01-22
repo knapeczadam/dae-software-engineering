f55c0b23b25ae42274b7ff06b558620c2a5686a3
Ádám Knapecz <knapecz.adam@gmail.com>
2025-01-22 11:19:53 +0100

    fix: test mouse's functionality

--- project/test/test_core.lua
+++ project/test/test_core.lua
@@ -7,6 +7,7 @@ local HEIGHT = 500
 local FRAME  = 50
 
 function initialize()
+    print("Initialize")
     GAME_ENGINE:set_title(TITLE)
     GAME_ENGINE:set_width(WIDTH)
     GAME_ENGINE:set_height(HEIGHT)
@@ -14,11 +15,11 @@ function initialize()
 end
 
 function start()
-
+    print("Start")
 end
 
 function on_destroy()
-
+    print("Destroy")
 end
 
 function paint(rect)
@@ -30,15 +31,15 @@ function tick()
 end
 
 function mouse_button_action(isLeft, isDown, x, y, param)
-
+    print("Mouse button: isLeft=" .. tostring(isLeft) .. ", isDown=" .. tostring(isDown) .. ", x=" .. x .. ", y=" .. y)
 end
 
 function mouse_wheel_action(x, y, distance, param)
-
+    print("Mouse wheel: x=" .. x .. ", y=" .. y .. ", distance=" .. distance)
 end
 
 function mouse_move(x, y, param)
-
+    print("Mouse move: x=" .. x .. ", y=" .. y)
 end
 
 function check_keyboard()
@@ -46,7 +47,7 @@ function check_keyboard()
 end
 
 function key_pressed(key)
-
+    print("Key pressed: " .. key)
 end
 
 function call_action(caller)
